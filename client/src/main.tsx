5f597e02fbe1ded858c8eaad8ec477dd42be7285
twillecke <90877406+twillecke@users.noreply.github.com>
2024-06-09 14:42:46 -0300

    change: add mantine ui package and config; remove tailwind css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App.js'
-import './components/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App.js';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <MantineProvider>{<App />}</MantineProvider>;
+    </React.StrictMode>
+);
